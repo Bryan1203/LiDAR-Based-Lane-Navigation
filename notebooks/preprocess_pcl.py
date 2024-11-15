a10dfd3d8c4d1576ca7807e79d6def4a152caef4
Bryan1203 <bryanchang1234@gmail.com>
2024-11-15 11:13:31 -0600

    pointcloud filtering

--- notebooks/preprocess_pcl.py
+++ notebooks/preprocess_pcl.py
@@ -7,34 +7,34 @@ import std_msgs.msg
 
 def point_cloud_callback(msg):
     # Desired height threshold
-    height_threshold = 2.0  # Adjust this threshold as needed
-    distance_threshold = 10
+    height_threshold = -1.0  # Adjust this threshold as needed
+    distance_threshold = 30
 
     # List to store filtered points
     filtered_points = []
-
+    total_points = 0
     # Read points from the original PointCloud2 message
-    for point in pc2.read_points(msg, skip_nans=True, field_names=("x", "y", "z", "near_ir")):
-        x, y, z, near_ir = point
-
+    for point in pc2.read_points(msg, skip_nans=True):
+        x, y, z, = point[0],point[1],point[2]
+        total_points+=1
         # Filter out points above the height threshold
-        if z <= height_threshold and (x**2+y**2+z**2)**(1/2) <= distance_threshold:
-            filtered_points.append((x, y, z, near_ir))
-
+        if z <= height_threshold and (x**2+y**2+z**2)**(1/2) <= distance_threshold and abs(y)<=10 and abs(x)<=20:
+            filtered_points.append(point)
     # Create a new PointCloud2 message for the filtered data
     header = std_msgs.msg.Header()
     header.stamp = rospy.Time.now()
     header.frame_id = msg.header.frame_id
-
     # Use the same fields as the original message
-    fields = msg.fields
-
+    # fields = [msg.fields[i] for i in [0,1,2,7]]
+    
+    print(msg.fields)
+    
     # Convert the filtered points to a PointCloud2 message
-    filtered_msg = pc2.create_cloud(header, fields, filtered_points)
+    filtered_msg = pc2.create_cloud(header, msg.fields, filtered_points)
 
     # Publish or store the filtered PointCloud2 message
     filtered_pub.publish(filtered_msg)
-    rospy.loginfo("Published filtered point cloud with height <= %.2f", height_threshold)
+    print(f"{len(filtered_points)}/{total_points}")
 
 def main():
     # Initialize the ROS node
